23c2e517f40b828e8fa54ee62c6c945c69d3ce62
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2021-04-11 14:26:31 +0200

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const PIXI = require('pixi.js');
 const $ = require('jquery');
 require('./keys.js');
 
+function isFunction(functionToCheck) {
+   return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
+}
+
 const main = function() {
 
   const grid = {};
@@ -48,8 +52,64 @@ const main = function() {
   $("body").css("padding", 0);
   //$("body").css("background", "radial-gradient(#c0c0c0, #ffffff)");
   $("body").css("background", "radial-gradient(#ffffff, #c0c0c0)");
-  $("canvas").css("padding-left", ipx * 0.5 * (width - minwh) + "px");
-  $("canvas").css("padding-top", ipx * 0.5 * (height - minwh) + "px");
+  const padleft = ipx * 0.5 * (width - minwh);
+  $("canvas").css("padding-left", padleft + "px");
+  const padtop =  ipx * 0.5 * (height - minwh);
+  $("canvas").css("padding-top", padtop + "px");
+
+  $("body").append("<div id='leftpanel'>No mouse</div>");
+  const leftpanel = $("#leftpanel");
+  leftpanel.css("position", "absolute");
+  leftpanel.css("left", (0.5 * padleft) + "px");
+  leftpanel.css("top", padtop + "px");
+  leftpanel.append("<ol id='ui'></ol>");
+
+  const ui = $("#ui");
+
+  ui.append('<li><ol id="ol1"></ol></li>');
+  ui.append('<li><ol id="ol2"></ol></li>');
+  ui.append('<li><ol id="ol3"></ol></li>');
+  ui.append('<li><ol id="ol4"></ol></li>');
+
+  const ol1 = $("#ol1");
+  const ol2 = $("#ol2");
+  const ol3 = $("#ol3");
+  const ol4 = $("#ol4");
+
+  ol1.append('<li><div style="display: inline-block; transform: rotate(0deg);">&#8593;</span></li>');
+  ol1.append('<li><div style="display: inline-block; transform: rotate(90deg);">&#8593;</span></li>');
+  ol1.append('<li><div style="display: inline-block; transform: rotate(180deg);">&#8593;</span></li>');
+  ol1.append('<li><div style="display: inline-block; transform: rotate(270);">&#8593;</span></li>');
+
+  ol2.append('<li><div style="display: inline-block; transform: rotate(0deg);">&#10548;</span></li>');
+  ol2.append('<li><div style="display: inline-block; transform: rotate(90deg);">&#10548;</span></li>');
+  ol2.append('<li><div style="display: inline-block; transform: rotate(180deg);">&#10548;</span></li>');
+  ol2.append('<li><div style="display: inline-block; transform: rotate(270deg);">&#10548;</span></li>');
+
+  ol3.append('<li><div style="display: inline-block; transform: scaleX(-1) rotate(0deg);">&#10548;</span></li>');
+  ol3.append('<li><div style="display: inline-block; transform: scaleX(-1) rotate(90deg);">&#10548;</span></li>');
+  ol3.append('<li><div style="display: inline-block; transform: scaleX(-1) rotate(180deg);">&#10548;</span></li>');
+  ol3.append('<li><div style="display: inline-block; transform: scaleX(-1) rotate(270deg);">&#10548;</span></li>');
+    
+  ol4.append('<li><div style="display: inline-block; transform: scaleX(-1) rotate(270deg);">&#129470;</span></li>');
+  
+
+  //const paths = {};
+  const selections = {};
+  const listItems = ui.find('li');
+  for (let li = 0; li < listItems.length; li++) {
+    const item = $(listItems[li]);
+    const parents = item.parents('li');
+    const parentNode = $(parents[parents.length - 1]);
+    const s = 
+      (parents.length > 0 ? parentNode.data('path') + '.' : '') +
+        (item.index() + 1).toString();
+    // paths[item] = s;
+    item.data('path', s);
+    selections[s] = item;
+    console.log(parentNode, item, item.index() + 1);
+  }
+  console.log(selections);
 
   const rootContainer = new PIXI.Container();
 
@@ -119,21 +179,47 @@ const main = function() {
       }
     }
   };
+  const selected = [];
   const processInput = function() {
     const delay = 0.1 * 1000;
     processKey(KeyEvent.DOM_VK_LEFT, delay, () => { return cursor.x--; });
     processKey(KeyEvent.DOM_VK_RIGHT, delay, () => { return cursor.x++; });
     processKey(KeyEvent.DOM_VK_UP, delay, () => { return cursor.y--; });
     processKey(KeyEvent.DOM_VK_DOWN, delay, () => { return cursor.y++; });
+    for (let i = 0; i < 10; i++) {
+      const s = i.toString();
+      processKey(KeyEvent['DOM_VK_' + s], delay, () => {
+        selected.push(i); 
+        let ss = selected.join('.');
+        const selection = selections[ss];
+        if (selection === undefined) {
+          selected.length = 0;
+          selected.push(i); 
+        }
+        console.log(selected.join('.'));
+      });
+    }
   }
-
   document.addEventListener('keydown', keydown);
   document.addEventListener('keyup', keyup);
+
+  console.log(ui);
+  const showSelected = function() {
+    $(".selected").removeClass("selected");
+    let ss = selected.join('.');
+    const selection = selections[ss];
+    if (selection === undefined) {
+      selected.length = 0;
+    } else {
+      selection.addClass("selected");
+    }
+  };
 
   app.ticker.add((delta) => {
     processInput();
     repaint(cursor.x, cursor.y, false);
     repositionAvatar();
+    showSelected();
   });
 
 };
